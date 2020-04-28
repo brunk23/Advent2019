2e1ac03ccc5994e15b707c16bbc0bc54dd899391
Kevin Brunelle <brunellek@gmail.com>
2020-04-28 04:57:39 +0000

    Refactor a little to make it cleaner

--- d10-2.c
+++ d10-2.c
@@ -7,13 +7,11 @@
 #define SIZE HEIGHT * WIDTH
 #define MAXRISE HEIGHT + 1
 #define MAXDIST (WIDTH + 1) * (WIDTH + 1) + (HEIGHT + 1) * (HEIGHT + 1)
+#define X 11
+#define Y 13
 
 /*
  * (11,13) is the point we will work from.
- */
-
-/*
- * This idea here is to draw lines from each point through all the edge points.
  */
 
 typedef struct Slope Slope;
@@ -42,14 +40,14 @@ Location
 	int cansee;
 };
 
-int gcd(int, int);
-void find_counts( Location map[][] );
-int howmany( Location map[][], int x, int y );
-void insert(int, int, int, int);
-int compare_slope(Slope *a, Slope *b);
-void vaporize(Slope *, int);
 Slope *find_next( Location map[HEIGHT][WIDTH], Slope *p, int run);
+int compare_slope(Slope *a, Slope *b);
+int gcd(int, int);
+int how_many( Location map[][], int x, int y );
 int verify( Slope *s, int run );
+void run_laser( Location map[][] );
+void insert(int, int, int, int);
+void vaporize(Slope *, int);
 
 void
 vaporize(Slope *s, int i)
@@ -114,20 +112,20 @@ insert(int rise, int run, int x, int y)
 	run /= i;
 
 	for( i = 0; i < SIZE; i++ ) {
-	if( !s[i].used ) {
-		s[i].rise = rise;
-		s[i].run = run;
-		s[i].distance = dist;
-		s[i].used = 1;
-		s[i].x = x;
-		s[i].y = y;
-		return;
-	}
+		if( !s[i].used ) {
+			s[i].rise = rise;
+			s[i].run = run;
+			s[i].distance = dist;
+			s[i].used = 1;
+			s[i].x = x;
+			s[i].y = y;
+			return;
+		}
 	}
 }
 
 int
-howmany( Location map[HEIGHT][WIDTH], int x, int y )
+how_many( Location map[HEIGHT][WIDTH], int x, int y )
 {
 	int a, b, rise, run;
 
@@ -173,25 +171,24 @@ verify( Slope *s, int run )
 Slope *
 find_next( Location map[HEIGHT][WIDTH], Slope *p, int run)
 {
-	int x, y;
-	Slope curr;
+	int x, y, distance;
 	Slope *currptr;
 
-	curr.distance = MAXDIST;
+	distance = MAXDIST;
 	currptr = nil;
 
 	for( x = 0; x < SIZE; x++ ) {
 		if( s[x].used && verify( &s[x], run ) ) {
 			if( (y = compare_slope( &s[x], currptr )) == 0 ) {
-				if( s[x].distance < curr.distance ) {
+				if( s[x].distance < distance ) {
 					currptr = &s[x];
-					curr.distance = s[x].distance;
+					distance = s[x].distance;
 				}
 			} else {
 				if( y == 1 ) {
 					if( !p || (compare_slope( &s[x], p ) == -1) ) {
 						currptr = &s[x];
-						curr.distance = s[x].distance;
+						distance = s[x].distance;
 					}
 				}
 			}
@@ -205,20 +202,20 @@ find_next( Location map[HEIGHT][WIDTH], Slope *p, int run)
  * This will find each asteroid and count how many it can see.  Then destroys them.
  */
 void
-find_counts( Location map[HEIGHT][WIDTH] )
+run_laser( Location map[HEIGHT][WIDTH] )
 {
 	int count = 0;
 	Slope *currptr, *prevptr;
 
-	map[13][11].cansee = howmany( map, 11, 13 );
+	map[Y][X].cansee = how_many( map, X, Y );
 
-	while( map[13][11].cansee ) {
+	while( map[Y][X].cansee ) {
 		prevptr = nil;
 		/* start by looking directly up */
 		currptr = find_next(map, prevptr, up);
 		if( currptr ) {
 			count++;
-			map[13][11].cansee--;
+			map[Y][X].cansee--;
 			vaporize( currptr, count );
 		}
 
@@ -228,7 +225,7 @@ find_counts( Location map[HEIGHT][WIDTH] )
 			currptr = find_next(map, prevptr, right);
 			if( currptr ) {
 				count++;
-				map[13][11].cansee--;
+				map[Y][X].cansee--;
 				vaporize( currptr, count );
 				prevptr = currptr;
 			} else {
@@ -241,7 +238,7 @@ find_counts( Location map[HEIGHT][WIDTH] )
 		currptr = find_next(map, prevptr, down);
 		if( currptr ) {
 			count++;
-			map[13][11].cansee--;
+			map[Y][X].cansee--;
 			vaporize( currptr, count );
 		}
 
@@ -251,7 +248,7 @@ find_counts( Location map[HEIGHT][WIDTH] )
 			currptr = find_next(map, prevptr, left);
 			if( currptr ) {
 				count++;
-				map[13][11].cansee--;
+				map[Y][X].cansee--;
 				vaporize( currptr, count );
 				prevptr = currptr;
 			} else {
@@ -289,21 +286,21 @@ main(int argc, char *argv[])
 		exits("Could not open file.");
 	}
 
-	for( x = 0; x < HEIGHT; x++ ) {
+	for( y = 0; y < HEIGHT; y++ ) {
 		len = fread( inp, sizeof(char), WIDTH+1, fp );
 		if( len != (WIDTH+1) ) {
 			print("Failed to get complete line. %d\n", len );
 		}
-		for( y = 0; y < WIDTH; y++ ) {
-			map[x][y].cansee = 0;
-			if( inp[y] == '#' ) {
-				map[x][y].asteroid = 1;
+		for( x = 0; x < WIDTH; x++ ) {
+			map[y][x].cansee = 0;
+			if( inp[x] == '#' ) {
+				map[y][x].asteroid = 1;
 			} else {
-				map[x][y].asteroid = 0;
+				map[y][x].asteroid = 0;
 			}
 		}
 	}
-	find_counts( map );
+	run_laser( map );
 
 	fclose(fp);
 	exits(0);
