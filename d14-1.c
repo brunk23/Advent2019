2182a7dbc521b84bd2b8ed7cec1550ffea248490
Kevin Brunelle <brunellek@gmail.com>
2020-04-29 00:57:46 +0000

    Part 1 is done.

--- d14-1.c
+++ d14-1.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 #define MAXLEN 120
-#define MAXLEFT 10
+#define MAXLEFT 20
 #define NAMESIZE 6
 #define NUMBER 100
 
@@ -24,6 +24,8 @@ struct Token
 struct Elem
 {
 	char name[ NAMESIZE ];
+	int increment;
+	int amount_needed;
 	int amount_made;
 	Token needs[ MAXLEFT ];
 };
@@ -35,17 +37,83 @@ enum
 
 Elem *parse_line( char *n );
 void print_element( Elem * );
+void optimize( Elem *[] );
+void run_list( Elem *[] );
+
+void
+run_list( Elem *a[] )
+{
+	int change = 1, i, j, need, tmp;
+
+	while( change ) {
+		change = 0;
+		for( i = 0; i < NUMBER; i++ ) {
+			if( !a[i] ) {
+				break;
+			}
+			if( a[i]->amount_needed > a[i]->amount_made ) {
+				change = 1;
+				tmp = a[i]->amount_needed - a[i]->amount_made;
+				need = tmp / a[i]->increment;
+				if( tmp % a[i]->increment ) {
+					need++;
+				}
+				a[i]->amount_made += (need * a[i]->increment);
+				for( tmp = 0; tmp < MAXLEFT; tmp++ ) {
+					if( !a[i]->needs[tmp].amount ) {
+						break;
+					}
+					a[i]->needs[tmp].recipe->amount_needed += (need * a[i]->needs[tmp].amount);
+				}
+			}
+		}
+	}
+}
+
+void
+optimize( Elem *a[] )
+{
+	int i = 0, j = 0, n = 0;
+	char *s;
+	Elem *where;
+
+	for( i = 0; i < NUMBER; i++ ) {
+		if( !a[i] ) {
+			break;
+		}
+		s = a[i]->name;
+		where = a[i];
+		if( !strcmp( s, "FUEL" ) ) {
+			a[i]->amount_needed = 1;
+		}
+		for( j = 0; j < NUMBER; j++ ) {
+			if( !a[j] ) {
+				break;
+			}
+			for( n = 0; n < MAXLEFT; n++ ) {
+				if( 0 == a[j]->needs[n].amount ) {
+					break;
+				}
+				if( !strcmp( s, a[j]->needs[n].name ) ) {
+					a[j]->needs[n].recipe = where;
+				}
+			}
+		}
+	}
+}
+
 
 void
 print_element( Elem *e ) {
 	int i;
-	print("To make %d of %s, we need ", e->amount_made, e->name);
+	print("To make %d of %s, we need ", e->increment, e->name);
 	for( i = 0; i < MAXLEFT; i++ ) {
 		if( e->needs[i].amount > 0 ) {
 			print("%d of %s, ", e->needs[i].amount, e->needs[i].name);
 		}
 	}
 	print("\n");
+	print("Needed: %d and Made %d\n", e->amount_needed, e->amount_made);
 }
 
 Elem *parse_line(char *s)
@@ -54,19 +122,18 @@ Elem *parse_line(char *s)
 	int state = leftamount;
 	Elem *e = nil;
 
-	print("%d\n", sizeof(Elem));
-
 	if( !( e = malloc( sizeof(Elem) ) ) ) {
 		exits("Can't get memory.");
 	}
 
 	for( i = 0; i < MAXLEFT; i++ ) {
 		e->needs[i].amount = 0;
+		e->amount_made = 0;
+		e->amount_needed = 0;
 	}
 
 	i = 0;
 	s = strtok(s, ", \r\t\n");
-	print("[%s] ", s);
 	sscanf( s, "%d", &e->needs[i].amount);
 	state = leftword;
 
@@ -77,23 +144,19 @@ Elem *parse_line(char *s)
 		}
 		switch( state ) {
 		case leftamount:
-			print("[%s] ", s);
 			sscanf( s, "%d", &e->needs[i].amount);
 			state = leftword;
 			break;
 		case leftword:
-			print("[%s] ", s);
 			strncpy( e->needs[i].name, s, NAMESIZE);
 			state = leftamount;
 			i++;
 			break;
 		case rightamount:
-			print("[%s] ", s);
-			sscanf( s, "%d", &e->amount_made);
+			sscanf( s, "%d", &e->increment);
 			state = rightword;
 			break;
 		case rightword:
-			print("[%s] ", s);
 			strncpy( e->name, s, NAMESIZE);
 			state = unknown;
 			break;
@@ -102,8 +165,6 @@ Elem *parse_line(char *s)
 			break;
 		}
 	}
-	print("\n");
-
 
 	return e;
 }
@@ -124,11 +185,28 @@ main(int argc, char *argv[])
 	if( !( fp = fopen("d14-input", "r") ) ) {
 		exits("Can't open file");
 	}
-	i = 0;
+
+	if( !( all[0] = malloc( sizeof(Elem) ) ) ) {
+		exits("Can't get element.");
+	}
+
+	all[0]->increment = 1;
+	all[0]->amount_made = 0;
+	all[0]->amount_needed = 0;
+	strncpy( all[0]->name, "ORE", NAMESIZE);
+	for( i = 0; i < MAXLEFT; i++ ) {
+		all[0]->needs[i].amount = 0;
+		all[0]->needs[i].name[0] = 0;
+	}
+
+	i = 1;
 	while( (s = fgets( n, MAXLEN, fp )) ) {
 		all[i] = parse_line( s );
 		i++;
 	}
+
+	optimize( all );
+	run_list( all );
 
 	for( i = 0; i < NUMBER; i++ ) {
 		if( all[i] ) {
