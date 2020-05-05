ccbb0faaa01797bc0e351af59f0a8f431490bff6
Kevin Brunelle <brunellek@gmail.com>
2020-05-05 22:36:03 +0000

    Some code clean-up to get this more readable.

--- d14-1.c
+++ d14-1.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 #define MAXLEN 120
-#define MAXLEFT 20
+#define MAXLEFT 12
 #define NAMESIZE 6
-#define NUMBER 100
+#define NUMBER 65
 
 typedef struct Elem Elem;
 typedef struct Token Token;
@@ -39,14 +39,40 @@ Elem *parse_line( char *n );
 void print_element( Elem * );
 void optimize( Elem *[] );
 void run_list( Elem *[] );
+void make_more( Elem * );
 
+/* this global is just a helper to for part 2, to quickly increase the fuel needed */
 Elem *fuel;
+
+/*
+ * Splitting this out of run_list seems to have a performance hit, but
+ * increases readability and function cleanliness dramatically.
+ */
+void
+make_more( Elem *e )
+{
+	vlong tmp, need;
+
+	tmp = e->amount_needed - e->amount_made;
+	need = tmp / e->increment;
+	if( tmp % e->increment ) {
+		need++;
+	}
+
+	e->amount_made += (need * e->increment);
+
+	for( tmp = 0; tmp < MAXLEFT; tmp++ ) {
+		if( !( e->needs[tmp].amount ) ) {
+			break;
+		}
+		e->needs[tmp].recipe->amount_needed += (need * e->needs[tmp].amount);
+	}
+}
 
 void
 run_list( Elem *a[] )
 {
-	int change = 1, i, j;
-	vlong need, tmp;
+	int change = 1, i;
 
 	while( change ) {
 		change = 0;
@@ -56,18 +82,7 @@ run_list( Elem *a[] )
 			}
 			if( a[i]->amount_needed > a[i]->amount_made ) {
 				change = 1;
-				tmp = a[i]->amount_needed - a[i]->amount_made;
-				need = tmp / (vlong) a[i]->increment;
-				if( tmp % (vlong) a[i]->increment ) {
-					need++;
-				}
-				a[i]->amount_made += (need * (vlong) a[i]->increment);
-				for( tmp = 0; tmp < MAXLEFT; tmp++ ) {
-					if( !a[i]->needs[tmp].amount ) {
-						break;
-					}
-					a[i]->needs[tmp].recipe->amount_needed += (need * (vlong) a[i]->needs[tmp].amount);
-				}
+				make_more( a[i] );
 			}
 		}
 	}
@@ -84,20 +99,25 @@ optimize( Elem *a[] )
 		if( !a[i] ) {
 			break;
 		}
+
 		s = a[i]->name;
 		where = a[i];
+
 		if( !strcmp( s, "FUEL" ) ) {
 			a[i]->amount_needed = 1;
 			fuel = a[i];
 		}
+
 		for( j = 0; j < NUMBER; j++ ) {
 			if( !a[j] ) {
 				break;
 			}
+
 			for( n = 0; n < MAXLEFT; n++ ) {
 				if( 0 == a[j]->needs[n].amount ) {
 					break;
 				}
+
 				if( !strcmp( s, a[j]->needs[n].name ) ) {
 					a[j]->needs[n].recipe = where;
 				}
@@ -108,15 +128,19 @@ optimize( Elem *a[] )
 
 
 void
-print_element( Elem *e ) {
+print_element( Elem *e )
+{
 	int i;
+
 	print("To make %d of %s, we need ", e->increment, e->name);
+
 	for( i = 0; i < MAXLEFT; i++ ) {
 		if( e->needs[i].amount > 0 ) {
 			print("%d of %s, ", e->needs[i].amount, e->needs[i].name);
 		}
 	}
 	print("\n");
+
 	print("Needed: %lld and Made %lld\n", e->amount_needed, e->amount_made);
 }
 
@@ -148,6 +172,10 @@ Elem *parse_line(char *s)
 		}
 		switch( state ) {
 		case leftamount:
+			if( i >= MAXLEFT ) {
+				print("ERROR: Out of room in Left\n");
+				exits("Out of left room");
+			}
 			sscanf( s, "%d", &e->needs[i].amount);
 			state = leftword;
 			break;
@@ -181,10 +209,11 @@ main(int argc, char *argv[])
 	char n[MAXLEN];
 	Elem *all[NUMBER];
 	int i;
-	vlong max;
+	vlong max = 1000;
 
-	max = 1000 * 1000;
-	max = max * max;
+	/* The compiler won't read 1 trillion correctly, so calculate it */
+	max *= max;
+	max *= max;
 
 
 	for( i = 0; i < NUMBER; i++ ) {
@@ -227,7 +256,7 @@ main(int argc, char *argv[])
 		}
 	}
 
-	print("Fuel: %lld\n",fuel->amount_needed - 1);
+	print("Fuel: %lld\n",fuel->amount_made);
 
 	for( i = 0; i < NUMBER; i++ ) {
 		if( all[i] ) {
